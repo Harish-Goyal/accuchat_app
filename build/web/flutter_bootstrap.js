5c09026a27bc2f37b3483a624d2d2650b1895a3d
muskan <muskanaccutech@gmail.com>
2025-11-12 16:37:58 +0530

    Update 3001
    Update 3001

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4021452957"
+    serviceWorkerVersion: "64885365"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -617,14 +617,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e72480e2bdc0c54a2c537a0093e99fc6
 "favicon.png": "e7b0966193fe97c600b0e1f2c460ba1e",
 "firebase-messaging-sw.js": "1f22297a1edb0324142e212c26e19900",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "6bd3af78c07f280c23d69ae73bef3cf3",
+"flutter_bootstrap.js": "ab5d9fdae5cbcbd71a13486820f3c694",
 "icons/Icon-192.png": "36f6fb3645e6382e5671f2944098f43c",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "abd44e71f6ed70a036c755f83357c0dc",
 "/": "abd44e71f6ed70a036c755f83357c0dc",
-"main.dart.js": "8337a517ddfdc2604db9368449d4c4af",
+"main.dart.js": "bcfe28cf48e7fd77d50fc1ef7c238365",
 "manifest.json": "9cbba53907fa21f47602ffbcb6f80bed",
 "version.json": "0af01196d05d5cf8f9687f1fd67a3dd7"};
 // The application shell files that are downloaded before a service worker can
