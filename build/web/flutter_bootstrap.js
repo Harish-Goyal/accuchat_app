eb91a80487075df03fd40e4ba2b008711256615e
muskan <muskanaccutech@gmail.com>
2025-11-13 10:18:34 +0530

    web latest build
    web latest build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,10 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-<<<<<<< HEAD
-    serviceWorkerVersion: "1123624621"
-=======
-    serviceWorkerVersion: "64885365"
->>>>>>> wor_web_06
+    serviceWorkerVersion: "2927161411"
   }
 });
